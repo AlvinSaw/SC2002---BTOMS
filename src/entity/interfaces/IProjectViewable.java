6f6024749406ff00653243d791941c22c2e103e9
AlvinSaw <136590429+AlvinSaw@users.noreply.github.com>
2025-04-24 22:27:24 +0800

    Updated java doc

--- src/entity/interfaces/IProjectViewable.java
+++
@@ -1,9 +0,0 @@
-package entity.interfaces;
-
-import entity.BTOProject;
-import java.util.List;
-
-public interface IProjectViewable {
-    List<BTOProject> getViewableProjects();
-    boolean canViewProject(BTOProject project);
-} 
